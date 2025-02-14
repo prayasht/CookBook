d8812437fd65577a44a7c7f0062791e843e530b1
prayash <thatoiprayash@gmail.com>
2025-02-14 16:55:07 +0530

    add  navbar reciple list syle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-
 import React from "react";
 
-function Navbar(){
-    return (
-        <>
-            <nav>
-            <div class="logo"><a href="#">Cook-Book</a></div>
-            <div class="nav-links">
-            <a id="AllRecipe" href="#">All Recipes</a>
-            <a id="MyRecipe" href="#">My Recipes</a>
-            </div>
-            </nav>
-        </>
-    )
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-gray-800 mt-2 p-4 rounded-lg">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white text-2xl font-bold">
+            <a href="#">Cook-Book</a>
+          </div>
+          <div className="flex justify-center">
+            <a id="AllRecipe" href="#" className="text-gray-300 hover:text-white">
+              All Recipes
+            </a>
+            <a id="MyRecipe" href="#" className="text-gray-300 hover:text-white">
+              My Recipes
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-function RecipePreview(){
-    return (
-        <>
-            <div className="Recipe-Prev">
-                <img className="Recipe-Image" src=""></img>
-                <h3 className="Recipe-Title">Not Added Yet</h3>
-                <span className="Recipe-Rating">*****</span>
-                <button className="Share-btn">--</button>
-            </div>
-        </>
-    )
+function RecipePreview() {
+  return (
+    <>
+      <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-sm mx-auto">
+        <img className="w-full h-48 object-cover" src="" alt="Recipe" />
+        <div className="p-4">
+          <h3 className="text-lg font-semibold text-gray-800">Not Added Yet</h3>
+          <span className="text-yellow-500">★★★★★</span>
+          <button className="bg-blue-500 text-white py-2 px-4 mt-4 rounded hover:bg-blue-700">
+            Share
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default RecipePreview;
